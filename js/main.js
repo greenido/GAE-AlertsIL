027074319c31cda0161a5ae7f9f988e47d9ef459
Ido Green <greenido@gmail.com>
2015-01-19 10:14:18 +0200

    New padding function - just in case

--- js/main.js
+++ js/main.js
@@ -74,6 +74,47 @@ timeAgo = function(dateString) {
     }
   };
 
+
+//
+// Helper function for padding of the news/alerts
+//
+var STR_PAD_LEFT = 1;
+var STR_PAD_RIGHT = 2;
+var STR_PAD_BOTH = 3;
+
+function pad(str, len, pad, dir) {
+
+    if (typeof(len) == "undefined") { var len = 0; }
+    if (typeof(pad) == "undefined") { var pad = ' '; }
+    if (typeof(dir) == "undefined") { var dir = STR_PAD_RIGHT; }
+
+    if (len + 1 >= str.length) {
+
+        switch (dir){
+
+            case STR_PAD_LEFT:
+                str = Array(len + 1 - str.length).join(pad) + str;
+            break;
+
+            case STR_PAD_BOTH:
+                var right = Math.ceil((padlen = len - str.length) / 2);
+                var left = padlen - right;
+                str = Array(left+1).join(pad) + str + Array(right+1).join(pad);
+            break;
+
+            default:
+                str = str + Array(len + 1 - str.length).join(pad);
+            break;
+
+        } // switch
+
+    }
+
+    return str;
+
+}
+//
+//
 //
 function fetchFeed(curFeed, curSource) {
   $.ajax({
@@ -96,6 +137,7 @@ function fetchFeed(curFeed, curSource) {
         }
         when += " (" + curSource + ")";
         // console.log("-----------" + when + "-------------");
+        
         var buttonHTML = '<div class="row">' + 
           '<div class="large-8 columns">' + entry.title + '</div>' +
           '<div class="small-8 small-centered columns round secondary"><span class="smallfont">' + when +
