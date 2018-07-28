2dcaf6a29b3d340f1c9f0f2811d4b622d52e4c2a
Ido Green <greenido@gmail.com>
2018-07-27 18:30:26 -0700

    initial version

---
+++ open-alerts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+#if [ $# -eq 0 ]
+#  then
+#    echo "No url supplied... please type something like: ./open-alerts.sh https://https://ido-green.appspot.com"
+#    exit 1;
+#fi
+
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --new-window --window-position=10,10 --window-size=600,800 https://alerts-il.appspot.com/
